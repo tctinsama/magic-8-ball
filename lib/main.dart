a99acade23b02a4365f9b39c18ae9c46b61ee83c
tctinsama <work.tctin@gmail.com>
2024-10-01 19:48:01 +0700

    Commit message

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
 
-class MyApp extends StatelessWidget {
+class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.lightBlue,
-        body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/avatar.jpg'),
-                ),
-                Text(
-                  'Trần Chánh Tín',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    color: Colors.lightBlue.shade50,
-                    fontSize: 20.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                  width: 150.0,
-                  child: Divider(
-                    color: Colors.lightBlue.shade100,
-                  ),
-                ),
-                Card(
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.lightBlue,
-                      ),
-                      title: Text(
-                        '+84 039 679 8864',
-                        style: TextStyle(
-                          color: Colors.lightBlue.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    )),
-                Card(
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.lightBlue,
-                      ),
-                      title: Text(
-                        'tintc.21it@vku.udn.vn',
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.lightBlue.shade900,
-                            fontFamily: 'Source Sans Pro'),
-                      ),
-                    ))
-              ],
-            )),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.green,
+      ),
+      body: Ball(),
+      backgroundColor: Colors.lightGreenAccent,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int answer = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: TextButton(
+          child: Image.asset('images/ball$answer.png'),
+          onPressed: () {
+            setState(() {
+              answer = Random().nextInt(5) + 1;
+            });
+          },
+        ),
       ),
     );
   }
